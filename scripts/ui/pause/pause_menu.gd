5bdde81658a9eddeea438fce79563f506b3b7382
Eusébio Alves <eusebio.a95@hotmail.com>
2024-09-16 11:26:15 +0100

    Basic pause functionality
    Button signal connected.

--- scripts/ui/pause/pause_menu.gd
+++ scripts/ui/pause/pause_menu.gd
@@ -3,7 +3,16 @@ class_name PauseMenu
 
 @onready var return_button = $PanelContainer/MarginContainer/ReturnButton
 
-
 func _input(event):
 	if event.is_action_pressed("pause"):
-		visible = !visible
+		pause_toggle()
+		
+func pause_toggle():
+	if visible:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		get_tree().paused = false
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		get_tree().paused = true
+	
+	visible = !visible
