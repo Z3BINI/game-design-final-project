f01f135e72f683d01bbf4b11059a2743e444f5a7
Eusébio Alves <eusebio.a95@hotmail.com>
2024-09-17 21:55:27 +0100

    Import map art & fix self harm

--- scenes/quack_world/components/damage_component.gd
+++ scenes/quack_world/components/damage_component.gd
@@ -2,7 +2,9 @@ extends Area2D
 class_name DamageComponent
 
 @export var DMG_AMOUNT : float = 2
+@export var can_dmg_player : bool = false
 
 func _on_area_entered(area : HealthComponent) -> void:
-	print(self.name)
+	if area.get_parent() is PlayerQuack and !can_dmg_player:
+		return	
 	area.take_dmg(DMG_AMOUNT)
