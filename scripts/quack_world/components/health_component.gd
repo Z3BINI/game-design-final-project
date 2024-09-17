fdf29265369a0298a0d7c0c71866f488a4934aab
Eusébio Alves <eusebio.a95@hotmail.com>
2024-09-17 22:28:45 +0100

    UI HP

--- scripts/quack_world/components/health_component.gd
+++ scripts/quack_world/components/health_component.gd
@@ -6,15 +6,19 @@ signal took_dmg
 signal died
 
 @export var MAX_HP : float = 10
+@export var health_bar : TextureProgressBar
 
 var current_hp : float
 
 func _ready() -> void:
 	current_hp = MAX_HP
+	health_bar.max_value = MAX_HP
+	health_bar.value = current_hp
 
 func take_dmg(amount : float) -> void:
 	print(self.name," took ", amount," damage!")
 	current_hp -= amount
+	health_bar.value = current_hp
 	print(self.name," has ", current_hp," hp!")
 	took_dmg.emit()
 	
@@ -24,6 +28,7 @@ func take_dmg(amount : float) -> void:
 func heal(amount : float) -> void:
 	current_hp += amount
 	healed.emit()
+	health_bar.value = current_hp
 	
 	if current_hp > MAX_HP:
 		current_hp = MAX_HP
