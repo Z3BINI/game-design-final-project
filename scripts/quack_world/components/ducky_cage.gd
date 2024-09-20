6b667c8a44b16d7fbd5cee8344a69d98000c4fb6
Eusébio Alves <eusebio.a95@hotmail.com>
2024-09-20 14:03:23 +0100

    Duck release system

--- scripts/quack_world/components/ducky_cage.gd
+++ scripts/quack_world/components/ducky_cage.gd
@@ -1,27 +1,45 @@
 extends Node2D
 
 @export var pointer : PackedScene
+@export var release_time : float = 5
+
 var player : PlayerQuack
 var my_pointer
+var player_near : bool = false
+var player_can_release : bool = false
+var release_progress : float = 0
+
+@onready var ducky_pivot = $DuckyPivot
+@onready var release_progress_bar = $DuckyPivot/ReleaseProgress
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	release_progress_bar.max_value = release_time
 	player = get_tree().get_first_node_in_group("player")
 	spawn_pointer()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func _physics_process(delta):
+	if player_near:
+		ducky_pivot.look_at(player.global_position)
+	
+	release_progress_bar.value = release_progress
+	release_progress_bar.visible = (release_progress > 0)
+	if Input.is_action_pressed("interact") and player_can_release:
+		release_progress += delta
+		
+		if release_progress >= release_time:
+			release_ducky()
+	else:
+		if release_progress > 0:
+			release_progress -= delta
 
 func _on_player_detector_body_entered(body: PlayerQuack) -> void:
-	body.enable_ducky(body.ducks)
-	body.ducks += 1
-	my_pointer.queue_free()
-	queue_free()
+	player_can_release = true
 
-
+func _on_player_detector_body_exited(body):
+	player_can_release = false
+	
 func spawn_pointer():
 	my_pointer = pointer.instantiate()
 	my_pointer.target = self
@@ -31,7 +49,15 @@ func spawn_pointer():
 
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
 	my_pointer.visible = false
+	player_near = true
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	my_pointer.visible = true
+	player_near = false
+
+func release_ducky():
+	player.enable_ducky(player.ducks)
+	player.ducks += 1
+	my_pointer.queue_free()
+	queue_free()
