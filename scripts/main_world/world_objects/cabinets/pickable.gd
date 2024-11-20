64f228f54af44e561c96fd99903d0313ddc2d9c0
Eusébio Alves <55258294+Z3BINI@users.noreply.github.com>
2024-11-20 10:11:38 +0000

    Pickable logic

--- scripts/main_world/world_objects/cabinets/pickable.gd
+++ scripts/main_world/world_objects/cabinets/pickable.gd
@@ -1,25 +1,37 @@
 extends RigidBody3D
 
-var player_hold_node : Marker3D = null
+signal pick_up
+signal drop
+
+var player_hold_node : Marker3D = null # Acts as player nearby bool as well
+var original_parent_node : Node3D
 var is_picked : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	original_parent_node = get_parent()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if (Input.is_action_just_pressed("interact") and player_hold_node != null and !is_picked):
-		global_position = player_hold_node.position
-	elif (Input.is_action_just_pressed("interact") and is_picked):
-		player_hold_node = null
-		is_picked = false
-		apply_central_impulse(Vector3.FORWARD * 100)
-
-
+func _input(event: InputEvent) -> void:
+	if (event.is_action_pressed("interact")):
+		if (player_hold_node != null and !is_picked):
+			is_picked = true
+			pick_up.emit()
+		else:
+			is_picked = false
+			drop.emit()
+	
 func _on_player_near_body_entered(body: Player3d) -> void:
 	player_hold_node = body.hold_position
 
 func _on_player_near_body_exited(body: Player3d) -> void:
 	player_hold_node = null
+	drop.emit() # Drop in case player buggs the item out
+	
+func _on_pick_up() -> void:
+	gravity_scale = 0
+	global_position = player_hold_node.global_position
+	reparent(player_hold_node)
+
+func _on_drop() -> void:
+	gravity_scale = 1
+	reparent(original_parent_node)
